2df629d83752322111148b69c7817e10574e3dbf
Julian Löhr <julian.loehr@outlook.com>
2015-01-16 15:04:01 +0100

    Added Min Max Property to DethMesh to clip values; improved color calculations

--- Assets/Script/DepthMesh.cs
+++ Assets/Script/DepthMesh.cs
@@ -8,23 +8,44 @@ public class DepthMesh : MonoBehaviour {
     uint KinectHeight = 240;
     uint Width;
     uint Height;
-
+    [HideInInspector]
     public Vector3[] newVertices;
+    [HideInInspector]
     public Vector3[] newNormals;
+    [HideInInspector]
     public Color32[] newColors;
+    [HideInInspector]
     public Vector2[] newUV;
+    [HideInInspector]
     public int[] newTriangles;
     Mesh MyMesh;
 
-    Kaliko.ImageLibrary.KalikoImage Image;
+    public int MinValue = 0;
+    public int MaxValue = short.MaxValue;
+
+    Kaliko.ImageLibrary.Filters.GaussianBlurFilter BlurFilter = new Kaliko.ImageLibrary.Filters.GaussianBlurFilter(2);
 
 	// Use this for initialization
     void Start()
     {
-        Image = new Kaliko.ImageLibrary.KalikoImage(320, 240);
-
         float Aspect = (float)KinectWidth / (float)KinectHeight;
-        //ushort.MaxValue
+        float SquarePixels = ushort.MaxValue * Aspect;
+        float FloatWidth = Mathf.Sqrt(SquarePixels);
+        float FloatHeight = FloatWidth / Aspect;
+        Width = (uint)Mathf.FloorToInt(FloatWidth);
+        Height = (uint)Mathf.FloorToInt(FloatHeight);
+
+        Debug.Log("W: " + Width + " - H: " + Height);
+            // W * H = ushort.MaxValue
+            // W = Aspect * H
+        //
+        // H = Max / W
+        // W = Aspect * max / W
+        // W² = Aspect * Max
+        // W = sqrt( Aspect * MAx)
+        // H = W / Aspect
+
+
 
         newVertices = new Vector3[Width * Height];
         newNormals = new Vector3[Width * Height];
@@ -72,21 +93,72 @@ public class DepthMesh : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        short MinValueBuffer = short.MaxValue;
+        short MaxValueBuffer = short.MinValue;
+
         if(KinectDepth.pollDepth())
         {
             short[] Img = KinectDepth.depthImg;
+            int[] ImageData = new int[Img.Length];
+
+            for (int i = 0; i < Img.Length; i++)
+            {
+                ImageData[i] = (int)((((int)Img[i]) << 8) | 0x000000FF);
+            }
+
+            Kaliko.ImageLibrary.KalikoImage Image = new Kaliko.ImageLibrary.KalikoImage((int)KinectWidth, (int)KinectHeight);
+            Image.IntArray = ImageData;
+            //
+            BlurFilter.Run(Image);
+            //Image.Resize((int)Width, (int)Height);
+            ImageData = Image.IntArray;
+            //Debug.Log(ImageData);
+
             for (uint H = 0; H < Height; H++)
             {
                 for (uint W = 0; W < Width; W++)
                 {
-                    uint ImgIndex = W + H * 320;
+                    uint ImgIndex = W + H * KinectWidth;
                     uint VertexIndex = W + H * Width;
-                    newVertices[VertexIndex].z = -1f * (Img[ImgIndex] / 32f);
-                    
+                    uint Value = (uint)(ImageData[ImgIndex] >> 8);
 
-                    byte R = (byte)((Mathf.Clamp(Img[ImgIndex] / 32, 127, 255) - 127) * 2);
-                    byte G = (byte)(Img[ImgIndex] / 32);
-                    byte B = (byte)(255 - (Mathf.Clamp(Img[ImgIndex]/ 32, 0, 127) * -2));
+                    if (Value > MaxValueBuffer)
+                    {
+                        MaxValueBuffer = (short)Mathf.Clamp(Value, Value, short.MaxValue);
+                    }
+
+                    if ((Value < MinValueBuffer) && (Value != 0))
+                    {
+                        MinValueBuffer = (short)Mathf.Clamp(Value, short.MinValue, Value);
+                    }
+
+                    if(Value == 0)
+                    {
+                        Value = (uint)short.MaxValue;
+                    }
+
+                    if(Value > MaxValue)
+                    {
+                        Value = (uint)MaxValue;
+                    }
+
+                    if(Value < MinValue)
+                    {
+                        Value = (uint)MinValue;
+                    }
+
+                    float FloatValue = (Value - MinValue) / (float)(MaxValue - MinValue);
+                    float FloatValueClamped = Mathf.Clamp01(FloatValue);
+                    byte ByteValue = (byte)Mathf.RoundToInt(FloatValue * byte.MaxValue);
+
+
+                    newVertices[VertexIndex].z = FloatValue * 100;
+
+                    //0-127 = 0 :: 127- 255 = 0 - 255
+                    byte R = (byte)(Mathf.Clamp((ByteValue - 127) * 2, 0, 255));
+                    //0 = 0; 127 = 255; 255 = 0
+                    byte G = (byte)(127 + (Mathf.Sign(127 - ByteValue) * ByteValue / 2));
+                    byte B = (byte)(255 - Mathf.Clamp(ByteValue * 2, 0, 255));
                     newColors[VertexIndex] = new Color32(R, G, B, 255);
                 }
             }
@@ -94,5 +166,7 @@ public class DepthMesh : MonoBehaviour {
             MyMesh.vertices = newVertices;
             MyMesh.colors32 = newColors;
         }
+
+        Debug.Log(MinValueBuffer + " - " + MaxValueBuffer);
 	}
 }
