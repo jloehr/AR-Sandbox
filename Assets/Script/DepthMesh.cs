489f67c4207b9420f665289e1e420c4014ebbafc
Julian Löhr <julian.loehr@outlook.com>
2015-01-23 12:29:34 +0100

    Refactored DeptpMesh

--- Assets/Script/DepthMesh.cs
+++ Assets/Script/DepthMesh.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DepthMesh : MonoBehaviour {
-
+public class DepthMesh : MonoBehaviour
+{
     public DepthWrapper KinectDepth;
-    uint KinectWidth = 320;
-    uint KinectHeight = 240;
-    uint Width;
-    uint Height;
+    int KinectWidth = 320;
+    int KinectHeight = 240;
     [HideInInspector]
     public Vector3[] newVertices;
     [HideInInspector]
@@ -20,24 +18,31 @@ public class DepthMesh : MonoBehaviour {
     public int[] newTriangles;
     Mesh MyMesh;
 
+    public int Width;
+    public int Height;
+    public int OffsetX;
+    public int OffsetY;
     public int MinValue = 0;
     public int MaxValue = short.MaxValue;
 
-    Kaliko.ImageLibrary.Filters.GaussianBlurFilter BlurFilter = new Kaliko.ImageLibrary.Filters.GaussianBlurFilter(2);
+    short MinValueBuffer;
+    short MaxValueBuffer;
+    short[] DepthImage;
+    float[] FloatValues;
 
-	// Use this for initialization
+    // Use this for initialization
     void Start()
     {
         float Aspect = (float)KinectWidth / (float)KinectHeight;
         float SquarePixels = ushort.MaxValue * Aspect;
         float FloatWidth = Mathf.Sqrt(SquarePixels);
         float FloatHeight = FloatWidth / Aspect;
-        Width = (uint)Mathf.FloorToInt(FloatWidth);
-        Height = (uint)Mathf.FloorToInt(FloatHeight);
+        Width = Mathf.FloorToInt(FloatWidth);
+        Height = Mathf.FloorToInt(FloatHeight);
 
         Debug.Log("W: " + Width + " - H: " + Height);
-            // W * H = ushort.MaxValue
-            // W = Aspect * H
+        // W * H = ushort.MaxValue
+        // W = Aspect * H
         //
         // H = Max / W
         // W = Aspect * max / W
@@ -46,37 +51,37 @@ public class DepthMesh : MonoBehaviour {
         // H = W / Aspect
 
 
-
+        FloatValues = new float[Width * Height];
         newVertices = new Vector3[Width * Height];
         newNormals = new Vector3[Width * Height];
         newColors = new Color32[Width * Height];
         newUV = new Vector2[Width * Height];
         newTriangles = new int[(Width - 1) * (Height - 1) * 6];
 
-        for (uint H = 0; H < Height; H++)
+        for (int H = 0; H < Height; H++)
         {
-            for (uint W = 0; W < Width; W++)
+            for (int W = 0; W < Width; W++)
             {
-                uint Index = W + H * Width;
+                int Index = W + H * Width;
                 newVertices[Index] = new Vector3(W, H, 0f);
                 newNormals[Index] = new Vector3(0, 0, 1);
                 newColors[Index] = new Color32(0, 0, 0, 255);
                 newUV[Index] = new Vector2(W / (float)Width, H / (float)Height);
 
-                if((W != (Width - 1)) && (H != (Height - 1)))
+                if ((W != (Width - 1)) && (H != (Height - 1)))
                 {
-                    uint TopLeft = Index;
-                    uint TopRight = Index + 1;
-                    uint BotLeft = Index + Width;
-                    uint BotRight = Index + 1 + Width;
+                    int TopLeft = Index;
+                    int TopRight = Index + 1;
+                    int BotLeft = Index + Width;
+                    int BotRight = Index + 1 + Width;
 
-                    uint TrinagleIndex = W + H * (Width - 1);
-                    newTriangles[TrinagleIndex * 6 + 0] = (int)TopLeft;
-                    newTriangles[TrinagleIndex * 6 + 1] = (int)BotLeft;
-                    newTriangles[TrinagleIndex * 6 + 2] = (int)TopRight;
-                    newTriangles[TrinagleIndex * 6 + 3] = (int)BotLeft;
-                    newTriangles[TrinagleIndex * 6 + 4] = (int)BotRight;
-                    newTriangles[TrinagleIndex * 6 + 5] = (int)TopRight;
+                    int TrinagleIndex = W + H * (Width - 1);
+                    newTriangles[TrinagleIndex * 6 + 0] = TopLeft;
+                    newTriangles[TrinagleIndex * 6 + 1] = BotLeft;
+                    newTriangles[TrinagleIndex * 6 + 2] = TopRight;
+                    newTriangles[TrinagleIndex * 6 + 3] = BotLeft;
+                    newTriangles[TrinagleIndex * 6 + 4] = BotRight;
+                    newTriangles[TrinagleIndex * 6 + 5] = TopRight;
                 }
             }
         }
@@ -89,84 +94,145 @@ public class DepthMesh : MonoBehaviour {
         MyMesh.colors32 = newColors;
         MyMesh.uv = newUV;
         MyMesh.triangles = newTriangles;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        short MinValueBuffer = short.MaxValue;
-        short MaxValueBuffer = short.MinValue;
+    }
 
-        if(KinectDepth.pollDepth())
+    // Update is called once per frame
+    void Update()
+    {
+        if (KinectDepth.pollDepth())
         {
-            short[] Img = KinectDepth.depthImg;
-            int[] ImageData = new int[Img.Length];
+            DepthImage = KinectDepth.depthImg;
+            CheckArrays();
+            CalculateFloatValues();
+            UpdateMesh();
+        }
+    }
 
-            for (int i = 0; i < Img.Length; i++)
+    void CheckArrays()
+    {
+
+    }
+
+   void CalculateFloatValues()
+    {
+        for (int H = 0; H < Height; H++)
+        {
+            for (int W = 0; W < Width; W++)
             {
-                ImageData[i] = (int)((((int)Img[i]) << 8) | 0x000000FF);
-            }
+                int ImageValue = GetImageValue(W, H);
+                int Index = GetArrayIndex(W, H);
 
-            Kaliko.ImageLibrary.KalikoImage Image = new Kaliko.ImageLibrary.KalikoImage((int)KinectWidth, (int)KinectHeight);
-            Image.IntArray = ImageData;
-            //
-            BlurFilter.Run(Image);
-            //Image.Resize((int)Width, (int)Height);
-            ImageData = Image.IntArray;
-            //Debug.Log(ImageData);
+                //Clamp Value
 
-            for (uint H = 0; H < Height; H++)
-            {
-                for (uint W = 0; W < Width; W++)
+                if (ImageValue > MaxValueBuffer)
                 {
-                    uint ImgIndex = W + H * KinectWidth;
-                    uint VertexIndex = W + H * Width;
-                    uint Value = (uint)(ImageData[ImgIndex] >> 8);
-
-                    if (Value > MaxValueBuffer)
-                    {
-                        MaxValueBuffer = (short)Mathf.Clamp(Value, Value, short.MaxValue);
-                    }
-
-                    if ((Value < MinValueBuffer) && (Value != 0))
-                    {
-                        MinValueBuffer = (short)Mathf.Clamp(Value, short.MinValue, Value);
-                    }
-
-                    if(Value == 0)
-                    {
-                        Value = (uint)short.MaxValue;
-                    }
-
-                    if(Value > MaxValue)
-                    {
-                        Value = (uint)MaxValue;
-                    }
-
-                    if(Value < MinValue)
-                    {
-                        Value = (uint)MinValue;
-                    }
-
-                    float FloatValue = (Value - MinValue) / (float)(MaxValue - MinValue);
-                    float FloatValueClamped = Mathf.Clamp01(FloatValue);
-                    byte ByteValue = (byte)Mathf.RoundToInt(FloatValue * byte.MaxValue);
-
-
-                    newVertices[VertexIndex].z = FloatValue * 100;
-
-                    //0-127 = 0 :: 127- 255 = 0 - 255
-                    byte R = (byte)(Mathf.Clamp((ByteValue - 127) * 2, 0, 255));
-                    //0 = 0; 127 = 255; 255 = 0
-                    byte G = (byte)(127 + (Mathf.Sign(127 - ByteValue) * ByteValue / 2));
-                    byte B = (byte)(255 - Mathf.Clamp(ByteValue * 2, 0, 255));
-                    newColors[VertexIndex] = new Color32(R, G, B, 255);
+                    MaxValueBuffer = (short)Mathf.Clamp(ImageValue, ImageValue, short.MaxValue);
                 }
+
+                if (ImageValue < MinValueBuffer)
+                {
+                    MinValueBuffer = (short)Mathf.Clamp(ImageValue, short.MinValue, ImageValue);
+                }
+
+                if (ImageValue > MaxValue)
+                {
+                    ImageValue = MaxValue;
+                }
+
+                if (ImageValue < MinValue)
+                {
+                    ImageValue = MinValue;
+                }
+
+                //Calculate
+                float FloatValue = (ImageValue - MinValue) / (float)(MaxValue - MinValue);
+                FloatValues[Index] = FloatValue;
             }
-            
-            MyMesh.vertices = newVertices;
-            MyMesh.colors32 = newColors;
         }
 
+   }
+
+    void UpdateMesh()
+    {
+        MinValueBuffer = short.MaxValue;
+        MaxValueBuffer = short.MinValue;
+
+        for (int H = 0; H < Height; H++)
+        {
+            for (int W = 0; W < Width; W++)
+            {
+                ProcessPixel(W, H);
+            }
+        }
+
+        MyMesh.vertices = newVertices;
+        MyMesh.colors32 = newColors;
+
         Debug.Log(MinValueBuffer + " - " + MaxValueBuffer);
-	}
+    }
+
+    void ProcessPixel(int W, int H)
+    {
+        int Index = GetArrayIndex(W, H);
+        float FloatValue = FloatValues[Index];
+        //Calc Normal
+
+        //Calc Position
+        newVertices[Index].z = FloatValue * 100;
+
+        //Calc Color
+        float FloatValueClamped = Mathf.Clamp01(FloatValue);
+        byte ByteValue = (byte)Mathf.RoundToInt(FloatValue * byte.MaxValue);
+
+        //0-127 = 0 :: 127- 255 = 0 - 255
+        byte R = (byte)(Mathf.Clamp((ByteValue - 127) * 2, 0, 255));
+        //0 = 0; 127 = 255; 255 = 0
+        byte G = (byte)(127 + (Mathf.Sign(127 - ByteValue) * ByteValue / 2));
+        byte B = (byte)(255 - Mathf.Clamp(ByteValue * 2, 0, 255));
+        newColors[Index] = new Color32(R, G, B, 255);
+    }
+
+    int GetImageValue(int W, int H)
+    {
+        int ImageW = OffsetX + W;
+        int ImageH = OffsetY + H;
+
+        if((ImageW < 0) || (ImageW > KinectWidth) || (ImageH < 0) || (ImageH > KinectHeight))
+        {
+            return (int)short.MaxValue;
+        }
+
+        int Index = ImageW + ImageH * KinectWidth;
+        int Value = DepthImage[Index];
+
+        if (Value == 0)
+        {
+            return (int)short.MaxValue;
+        }
+        else
+        {
+            return Value;
+        }
+    }
+    int GetArrayIndex(int W, int H)
+    {
+        return W + H * Width;
+    }
+
+    int[] ShortToRGBA(short[] DepthImage)
+    {
+        int[] ImageData = new int[DepthImage.Length];
+
+        for (int i = 0; i < DepthImage.Length; i++)
+        {
+            ImageData[i] = (int)((((int)DepthImage[i]) << 8) | 0x000000FF);
+        }
+
+        return ImageData;
+    }
+
+    int RGBAToShort(int Value)
+    {
+        return (Value >> 8);
+    }
 }
