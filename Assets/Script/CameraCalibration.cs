39630f441a7f9a3ac41085e3afad02a881f324f7
Julian Löhr <julian.loehr@outlook.com>
2015-02-09 16:41:04 +0100

    Added Small Camera Calibration Script

---
+++ Assets/Script/CameraCalibration.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCalibration : MonoBehaviour {
+
+
+    private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKey(KeyCode.Q))
+        {
+            camera.orthographicSize++;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            camera.orthographicSize--;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            camera.transform.position += Vector3.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            camera.transform.position += Vector3.down;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            camera.transform.position += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            camera.transform.position += Vector3.right;
+        }
+	}
+}
